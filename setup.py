2e5c244e91106d8d61622a2e85bf8205fb408170
OV DevOps <32085750+ovdevops@users.noreply.github.com>
2019-07-16 16:07:52 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 setup(name='cabot-alert-slack',
       version='0.8.3',
       description='A slack alert plugin for Cabot by Arachnys',
-      author='Luka Blaskovic',
-      author_email='lblasc@znode.net',
+      author='Hung Pham',
+      author_email='test@123.com',
       url='http://lblasc.znode.net',
       packages=find_packages(),
-      download_url = 'https://github.com/lblasc/cabot-alert-slack/tarball/0.6',
+      download_url = 'https://github.com/ovdevops/cabot-alert-slack.git',
      )
